4e9a2d7be4e301bc0b268ff6fda99767b72fc731
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2022-12-15 17:36:34 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -2,6 +2,8 @@ package SimpleCalc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel MainPanel;
@@ -11,15 +13,71 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField lblResult;
 
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.MainPanel);
-        app.setSize(800, 600);
+        app.setSize(700, 200);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
-        app.cbOperations.addItem("+");
-        app.cbOperations.addItem("-");
-        app.cbOperations.addItem("*");
-        app.cbOperations.addItem("/");
+    }
+
+    public double getNum1(){
+        return Double.parseDouble(tfNumber1.getText());
+    }
+
+    public double getNum2(){
+        return Double.parseDouble(tfNumber2.getText());
+    }
+
+    public SimpleCalcGUI(){
+        cbOperations.addItem("+");
+        cbOperations.addItem("-");
+        cbOperations.addItem("*");
+        cbOperations.addItem("/");
+        cbOperations.setSelectedIndex(0);
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String op = (String) cbOperations.getSelectedItem();
+                double result;
+
+                switch (op){
+                    case "+":
+                        result = getNum1() + getNum2();
+                        if(result % 1 == 0){
+                            lblResult.setText(String.valueOf((int)result));
+                            break;
+                        }
+                        lblResult.setText(String.valueOf(result));
+                        break;
+                    case "-":
+                        result = getNum1() - getNum2();
+                        if(result % 1 == 0){
+                            lblResult.setText(String.valueOf((int)result));
+                            break;
+                        }
+                        lblResult.setText(String.valueOf(result));
+                        break;
+                    case "*":
+                        result = getNum1() * getNum2();
+                        if(result % 1 == 0){
+                            lblResult.setText(String.valueOf((int)result));
+                            break;
+                        }
+                        lblResult.setText(String.valueOf(result));
+                        break;
+                    case "/":
+                        result = getNum1() / getNum2();
+                        if(result % 1 == 0){
+                            lblResult.setText(String.valueOf((int)result));
+                            break;
+                        }
+                        lblResult.setText(String.valueOf(result));
+                        break;
+                }
+            }
+        });
     }
 }
