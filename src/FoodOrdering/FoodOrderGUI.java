f9abe8bb12d497227247d95d661e82e0ca2b1a3c
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2022-12-16 11:26:40 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel MainPanel;
@@ -25,5 +27,58 @@ public class FoodOrderGUI extends JFrame{
         app.setVisible(true);
     }
 
+    public FoodOrderGUI(){
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double amount = 0;
+                double discount = 0;
+                if(cPizza.isSelected()){
+                    amount += 100;
+                }
 
+                if(cBurger.isSelected()){
+                    amount += 80;
+                }
+
+                if(cFries.isSelected()){
+                    amount += 65;
+                }
+
+                if(cSoftDrinks.isSelected()){
+                    amount += 55;
+                }
+
+                if(cTea.isSelected()){
+                    amount += 50;
+                }
+
+                if(cSundae.isSelected()){
+                    amount += 40;
+                }
+
+                if(rb5.isSelected()){
+                    discount = (double) amount * 0.05;
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(null, String.format("The total price is Php %.2f", amount));
+                }
+
+                if(rb10.isSelected()){
+                    discount = (double) amount * 0.10;
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(null, String.format("The total price is Php %.2f", amount));
+                }
+
+                if(rb15.isSelected()){
+                    discount = (double) amount * 0.15;
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(null, String.format("The total price is Php %.2f", amount));
+                }
+
+                if(rbNone.isSelected()){
+                    JOptionPane.showMessageDialog(null, String.format("The total price is Php %.2f", amount));
+                }
+            }
+        });
+    }
 }
